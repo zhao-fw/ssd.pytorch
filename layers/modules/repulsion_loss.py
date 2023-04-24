4920c10580d0fd7cb9e52eab26164e6b6be8250b
zhao-fw <zyf504044@163.com>
2023-04-24 13:34:16 +0800

    修改repulsion中的smoothln函数

--- layers/modules/repulsion_loss.py
+++ layers/modules/repulsion_loss.py
@@ -10,22 +10,30 @@ import sys
 
 
 class RepulsionLoss(nn.Module):
-
     def __init__(self, use_gpu=True, sigma=0.):
         super(RepulsionLoss, self).__init__()
         self.use_gpu = use_gpu
         self.variance = cfg['variance']
         self.sigma = sigma
 
-    # TODO
+    # TODO: 研究smoothln函数
     def smoothln(self, x, sigma=0.):
-        pass
+        self.sigma = 0
+        # 创建sigma张量
+        sigma = torch.full(x.shape, sigma)
+        if x <= sigma:
+            return -torch.log(1 - x + 1e-10)
+        else:
+            return (x - sigma) / (1 - sigma) - torch.log(1 - sigma)
 
     def forward(self, loc_data, ground_data, prior_data):
-        decoded_boxes = decode_new(loc_data, Variable(prior_data.data, requires_grad=False), self.variance)
+        decoded_boxes = decode_new(loc_data,
+                                   Variable(prior_data.data, requires_grad=False),
+                                   self.variance)
+        # 计算IoG
         iog = IoG(ground_data, decoded_boxes)
         # sigma = 1
         # loss = torch.sum(-torch.log(1-iog+1e-10))
         # sigma = 0
-        loss = torch.sum(iog)
+        loss = torch.sum(self.smoothln(iog))
         return loss
